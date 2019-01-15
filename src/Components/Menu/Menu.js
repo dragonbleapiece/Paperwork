5acaba0f44b43c8d7f8a4c63bcf3e5325d20e6c2
Quentin Sedmi <quentinsedmi@gmail.com>
2019-01-16 00:56:06 +0100

    Menu and MenuItem

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './Menu.css';
 
 class Menu extends Component {
+
   render() {
     return (
       <div className="Menu">
-        
+        {this.props.children}
       </div>
     );
   }
