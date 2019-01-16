479259f14a6eafbcf911c2967c3651d2c9902fac
dragonbleapiece <nicolas.cusumano0@gmail.com>
2019-01-16 10:28:19 +0100

    canvas

--- src/Components/Canvas/Canvas.js
+++ src/Components/Canvas/Canvas.js
@@ -1,7 +1,68 @@
 import React, { Component } from 'react';
+import p5 from 'p5';
 import './Canvas.css';
+import PropTypes from 'prop-types';
+
 
 class Canvas extends Component {
+
+  /*Singleton*/
+  static _instance;
+  static _P5;
+
+  constructor(props) {
+    if(Canvas._instance != undefined) {
+      return Canvas._instance;
+    }
+    super(props);
+
+    Canvas._instance = this;
+  }
+
+  setup(sk) {
+
+    const {width, height, cells} = this.props;
+
+    console.log(cells);
+
+    sk.createCanvas(width, height);
+  	var columns = sk.floor(width/cells);
+  	var rows = sk.floor(height/cells);
+
+  	sk.background(0);
+  	sk.stroke(255);
+  	for(var i = 0; i < columns; i++) {
+  		for(var j = 0; j < rows; j++) {
+  			sk.line(i * cells, 0, i * cells, height);
+  			sk.line(0, j * cells, width, j * cells);
+  		}
+  	}
+  }
+
+  draw(sk) {
+
+  }
+
+
+  componentDidMount() {
+    let s = (sk) => {
+      sk.setup = () => {
+        if(Canvas._instance != undefined) {
+          Canvas._instance.setup(sk);
+        }
+      }
+
+      sk.draw = () => {
+        if(Canvas._instance != undefined) {
+          Canvas._instance.draw(sk);
+        }
+      }
+    }
+
+    Canvas._P5 = new p5(s);
+  }
+
+
   render() {
     return (
       <div className="Canvas">
@@ -9,6 +70,16 @@ class Canvas extends Component {
       </div>
     );
   }
+}
+
+Canvas._instance = undefined;
+Canvas._P5 = undefined;
+
+/*PropTypes*/
+Canvas.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  cells: PropTypes.number
 }
 
 export default Canvas;
--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Canvas from '../../Components/Canvas/Canvas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Canvas width="800" height="400" cells="20"/>
       </div>
     );
   }
