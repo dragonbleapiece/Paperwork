ae3937f408ce2b7618d492a41faff1b23cf34612
Quentin Sedmi <quentinsedmi@gmail.com>
2019-05-15 00:44:31 +0200

    Examples in header and correct edition menu shrink

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -38,7 +38,7 @@ window.isAuthorized = function(type, unauthorized) {
 
   if(unauthorized instanceof Array) {
     let obj;
-    if(typeof type === "string") {
+    if(typeof type === 'string') {
       let cl = getClassFromName(type);
       obj = new cl();
     } else {
@@ -48,7 +48,7 @@ window.isAuthorized = function(type, unauthorized) {
     for(let i = 0; i < unauthorized.length && r; ++i) {
       let unauthorizedClass = getClassFromName(unauthorized[i]);
       if(unauthorizedClass === null) {
-        console.error(unauthorized[i] + " : Invalid unauthorized Class");
+        console.error(unauthorized[i] + ' : Invalid unauthorized Class');
         continue;
       }
       r = r && !(obj instanceof unauthorizedClass);
@@ -95,22 +95,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" onClick={(e) => this.handleOutsideClick(e)}>
-        <header className="App-header border-bottom">
-          <h1 className="App-logo">Paperwork&nbsp;<sup className="textExponent">2019</sup></h1>
-          <div className="about" ref={aboutButton => { this.aboutButton = aboutButton; }}>
+      <div className='App' onClick={(e) => this.handleOutsideClick(e)}>
+        <header className='App-header border-bottom'>
+          <h1 className='App-logo'>Paperwork&nbsp;<sup className='textExponent'>2019</sup></h1>
+          <div className='App-links'>
+          <a className='App-link' href='http://mobitool.free.fr/edu/ma-ex1.html' target='_blank' rel='noopener noreferrer'>Markov Example #1</a>
+          <a className='App-link' href='http://mobitool.free.fr/edu/ma-ex2.html' target='_blank' rel='noopener noreferrer'>Markov Example #2</a>
+          </div>
+          <div className='about' ref={aboutButton => { this.aboutButton = aboutButton; }}>
             <SVG src={icon_info}/>
           </div>
         </header>
         <main>
-          <div className="leftSide">
-              <div className="aboutContainer" ref={aboutContainer => { this.aboutContainer = aboutContainer; }}>
-                <h2 className="aboutTitle">PaperWork <sup className="textExponent">2019</sup></h2>
-                <a className="aboutLink" href="http://mobitool.free.fr/paper/ea" target="_blank" rel="noopener noreferrer">State of the arts online</a>
+          <div className='leftSide'>
+              <div className='aboutContainer' ref={aboutContainer => { this.aboutContainer = aboutContainer; }}>
+                <h2 className='aboutTitle'>PaperWork <sup className='textExponent'>2019</sup></h2>
+                <a className='aboutLink' href='http://mobitool.free.fr/paper/ea' target='_blank' rel='noopener noreferrer'>State of the arts online</a>
                 <p>Paperwork is an online software for research in art and in the field of computer and visual research. It is developed in the perspective of computer art practice in the context of art schools, colleges, high schools and so on. The application helps producing drawings in the philosophy of generative processes. It is concerned with patterns, polygons, repertories and grammars. Moreover, the interface enables the learning and the editing of algorithms and therefore the construction of simple computational processes. It also offers to export svg format that makes it compatible with the traditional vector work area (graphic design, pen plotter, laser cutting, ...).</p>
                 <p>Paperwork’s main algorithm is based on Markov chain, an algorithm that was used by artists such as Frieder Nake (Germany), Hiroshi Kawano (Japan), Hervé Huitric and Monique Nahas (France) in early Computer Art. It is now heavily used in our digital environment (google page ranking, chatbots, physical phenomenon modeling, ...). It is also classified in the machine learning area of the computational field.</p>
                 <p>The Paperwork web application is based on the previous software experiment Generic Images (2018). Paperwork’s project is developed thanks to IMAC-ESIPE training program in the University Paris-Est Marne-la-Vallée.</p>
-                <div className="aboutCredits">
+                <div className='aboutCredits'>
                 <p>Software design team:</p>
                 <p>Gaëtan Robillard with Nicolas Cusumano, Cécile Rousset, Vincent Schmid, Quentin Sedmi.</p>
                 </div>
@@ -119,8 +123,8 @@ class App extends Component {
 
               </Workspace>
           </div>
-            <div className="rightSide border-left">
-              <div className="editionMenu border-bottom">
+            <div className='rightSide border-left'>
+              <div className='editionMenu border-bottom'>
                 <Save/>
               </div>
                 <Canvas/>
