8e35e58a18aa63c8236340c755b653d282225d2c
dragonbleapiece <nicolas.cusumano0@gmail.com>
2019-09-19 13:50:53 +0200

    Updates

---
+++ precache-manifest.6f9c1948b6b1b285ef13c7c1759c383b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e6ac8d6049a92b9eadab9cc598eb23",
+    "url": "/PAPER/index.html"
+  },
+  {
+    "revision": "c19019b607a38bf3c870",
+    "url": "/PAPER/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "abea9455e826fe7938b5",
+    "url": "/PAPER/static/css/main.6bc2724d.chunk.css"
+  },
+  {
+    "revision": "c19019b607a38bf3c870",
+    "url": "/PAPER/static/js/2.1317e929.chunk.js"
+  },
+  {
+    "revision": "abea9455e826fe7938b5",
+    "url": "/PAPER/static/js/main.d5fa3824.chunk.js"
+  },
+  {
+    "revision": "06c00c66377a054976e9",
+    "url": "/PAPER/static/js/runtime~main.d46c468a.js"
+  },
+  {
+    "revision": "f3da2da9dea8e0f61f3ed1dd49f75e92",
+    "url": "/PAPER/static/media/HHatching.f3da2da9.svg"
+  },
+  {
+    "revision": "186b393d3d5fa55a5409806f1651c145",
+    "url": "/PAPER/static/media/LOHatching.186b393d.svg"
+  },
+  {
+    "revision": "8c0092fde14554b3813e960879b2580f",
+    "url": "/PAPER/static/media/ROHatching.8c0092fd.svg"
+  },
+  {
+    "revision": "9fa7b0a49e01a09dabb6ac6e3167a40c",
+    "url": "/PAPER/static/media/SpaceGrotesk-Regular.9fa7b0a4.ttf"
+  },
+  {
+    "revision": "c87660203b814d8e3c56011c2e658f87",
+    "url": "/PAPER/static/media/VHatching.c8766020.svg"
+  },
+  {
+    "revision": "f06acfc6598433894a74effa7652e3fa",
+    "url": "/PAPER/static/media/arrow_downUI.f06acfc6.svg"
+  },
+  {
+    "revision": "992a373bd8d1d30c158c85dcac357534",
+    "url": "/PAPER/static/media/arrow_right.992a373b.svg"
+  },
+  {
+    "revision": "79443cdd2f5ac354c1ef0450b359aef1",
+    "url": "/PAPER/static/media/arrow_rightUI.79443cdd.svg"
+  },
+  {
+    "revision": "d8be3f88ebe0fa31d562289dba77a90c",
+    "url": "/PAPER/static/media/arrow_upUI.d8be3f88.svg"
+  },
+  {
+    "revision": "38a76e4c4ab32d776faf4ec3a92543c0",
+    "url": "/PAPER/static/media/burst_mode.38a76e4c.svg"
+  },
+  {
+    "revision": "4afab8b8c3d5ebfa9e939fe3861fa248",
+    "url": "/PAPER/static/media/cancel.4afab8b8.svg"
+  },
+  {
+    "revision": "61cb41c4f7f3d3728c97e8c4b0c35f13",
+    "url": "/PAPER/static/media/full_ellipse.61cb41c4.svg"
+  },
+  {
+    "revision": "c61ab8ee87fc855460b18acb10af73ed",
+    "url": "/PAPER/static/media/full_square.c61ab8ee.svg"
+  },
+  {
+    "revision": "288a223997dbfe774fb7ab52c788c3f5",
+    "url": "/PAPER/static/media/full_triangle.288a2239.svg"
+  },
+  {
+    "revision": "8ace643bd20faf4ffd9980ccb894a48b",
+    "url": "/PAPER/static/media/grid_on.8ace643b.svg"
+  },
+  {
+    "revision": "b08ccdbec51af209390031e4f45d68b0",
+    "url": "/PAPER/static/media/info.b08ccdbe.svg"
+  },
+  {
+    "revision": "0f510805d8a717be2273446afe2dfd87",
+    "url": "/PAPER/static/media/next.0f510805.svg"
+  },
+  {
+    "revision": "73ed8e6297390a7a1ee61798428b4733",
+    "url": "/PAPER/static/media/one.73ed8e62.svg"
+  },
+  {
+    "revision": "5d1fc53d5965177b4dcb4a7cdfd4e2a8",
+    "url": "/PAPER/static/media/rotate.5d1fc53d.svg"
+  },
+  {
+    "revision": "3ccde9725bd42af98554d86dcaf681df",
+    "url": "/PAPER/static/media/save_alt.3ccde972.svg"
+  },
+  {
+    "revision": "08ae383667478c5cd53d365d875d8920",
+    "url": "/PAPER/static/media/scale.08ae3836.svg"
+  },
+  {
+    "revision": "aaf3d874ce455c2f3accb532ae59ebfb",
+    "url": "/PAPER/static/media/translate.aaf3d874.svg"
+  },
+  {
+    "revision": "3f5bae44c7c44909ed7895ec88ca77fa",
+    "url": "/PAPER/static/media/unknown.3f5bae44.svg"
+  },
+  {
+    "revision": "a2a55d7be479b8d287e4b00dac57c612",
+    "url": "/PAPER/static/media/void.a2a55d7b.svg"
+  },
+  {
+    "revision": "691bc51340078f640e5b29b078c56607",
+    "url": "/PAPER/static/media/zero.691bc513.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.01c777940d659a87caf74a52fa0c6a6f.js"
+  "/PAPER/precache-manifest.6f9c1948b6b1b285ef13c7c1759c383b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PAPER/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
